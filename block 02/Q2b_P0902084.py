3b8c39c034b1908146f67249183f964d4b6a9dd6
danielopq <danielopq@gmail.com>
2026-06-25 16:54:01 +0100

    TMA2 Question 2 fully done

--- block 02/Q2b_P0902084.py
+++ block 02/Q2b_P0902084.py
@@ -1,1 +1,22 @@
+# Verify whether the transformed card number is valid
+
+# Initialise the input list with the transformed card number
 processed_digits = [8, 9, 3, 9, 9, 0, 8, 7, 2, 2, 6, 4, 4, 0, 7, 4]
+
+# Initialise the aggregator to 0
+aggregator = 0
+
+# Calculate the sum of the digits     
+# For each digit in the list, Add the value of the digit to the aggregator
+for digit in processed_digits:
+    aggregator = aggregator + digit
+
+# Find the remainder when the aggregator is divided by 10
+reminder = aggregator % 10
+
+# Check whether the number is valid
+# If the remainder is not equal to the last digit, print ‘Number not valid'. Otherwise, print ‘Number valid’
+if reminder != processed_digits[len(processed_digits)-1]:
+    print('Number not valid')
+else:
+    print('Number valid')
--- block 02/Q2c_P0902084.py
+++ block 02/Q2c_P0902084.py
@@ -1,4 +1,4 @@
-# Apply the Luhn algorithm to a credit or debit card number
+# Check card number
 
 # Initialise a list containing the 16 digits of the card number
 card_number = [4,9,6,9,9,0,4,7,1,2,3,4,2,0,8,4]
@@ -8,6 +8,9 @@ processed_digits = [0] * len(card_number)
 
 # Initialise a variable for intermediate calculations
 result = 0
+
+# Initialise the aggregator to 0
+aggregator = 0
 
 # Apply the algorithm to each digit in the card number
 for position in range(len(card_number)):
@@ -29,5 +32,19 @@ for position in range(len(card_number)):
     else:
         processed_digits[position] = card_number[position]
 
-# Print the output list
-print(processed_digits)
+# Verify whether the transformed card number is valid
+
+# Calculate the sum of the digits     
+# For each digit in the list, Add the value of the digit to the aggregator
+for digit in processed_digits:
+    aggregator = aggregator + digit
+
+# Find the remainder when the aggregator is divided by 10
+reminder = aggregator % 10
+
+# Check whether the number is valid
+# If the remainder is not equal to the last digit, print ‘Number not valid'. Otherwise, print ‘The Number is valid.’
+if reminder != processed_digits[len(processed_digits)-1]:
+    print('Number not valid')
+else:
+    print('Number valid')
